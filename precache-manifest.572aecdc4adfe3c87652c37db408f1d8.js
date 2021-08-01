b757a8bf702fb137081df810fb62065e717c7378
Jongok Yoon <bak01076@gmail.com>
2021-08-01 14:55:45 +0200

    Updates

---
+++ precache-manifest.572aecdc4adfe3c87652c37db408f1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e432f9e65d04e9c07960c9a38020f463",
+    "url": "/react-habit-tracker/index.html"
+  },
+  {
+    "revision": "1368fa1ee98a40758a68",
+    "url": "/react-habit-tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "901412772f556135ac5d",
+    "url": "/react-habit-tracker/static/js/2.69a7904b.chunk.js"
+  },
+  {
+    "revision": "e79898a42ec2d02d5bbd03509f17e33d",
+    "url": "/react-habit-tracker/static/js/2.69a7904b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1368fa1ee98a40758a68",
+    "url": "/react-habit-tracker/static/js/main.b8307278.chunk.js"
+  },
+  {
+    "revision": "042765e3f71c043ed96c",
+    "url": "/react-habit-tracker/static/js/runtime-main.f843a2cf.js"
+  }
+]);
